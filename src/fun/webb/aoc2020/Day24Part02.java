063aafc5b9d59119b3c9025e2cdc52a2b5a210cc
webb <webbsemail@gmail.com>
2020-12-24 01:24:57 -0500

    completed day 24 part 2

--- src/fun/webb/aoc2020/Day24Part02.java
+++ src/fun/webb/aoc2020/Day24Part02.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Day24Part02 {
+
+    private static final String[] DIRECTIONS = new String[] {"e", "se", "sw", "w", "nw", "ne"};
+
     public static void main(String [] args) throws IOException {
         List<String> lines = Files.readAllLines(Path.of("inputs/day24.txt"));
         List<List<String>> instructions = new ArrayList<>();
@@ -32,22 +36,98 @@ public class Day24Part02 {
             pointCount.put(p, ++count);
         }
 
-        int blackTiles = 0;
-        for (Integer count : pointCount.values()) {
-            if (count % 2 == 1) blackTiles++;
+        Map<Point, Point> tileStates = new ConcurrentHashMap<>();
+        for (Map.Entry<Point, Integer> count : pointCount.entrySet()) {
+            Point p = count.getKey();
+            if (count.getValue() % 2 == 1) {
+                p.flipToBlack();
+                tileStates.put(p, p);
+            }
+            tileStates.put(p, p);
         }
 
-        System.out.println(blackTiles);
+        for (int day = 1; day <= 100; day++) {
+            tileStates = determineNextDayState(tileStates);
+        }
+        System.out.println(countBlackTiles(tileStates.keySet()));
+    }
+
+    private static Map<Point, Point> determineNextDayState(Map<Point, Point> tileState) {
+        Map<Point, Point> nextDayState = new ConcurrentHashMap<>();
+        for (Point p : tileState.keySet()) {
+            flipIfNeeded(tileState, nextDayState, p);
+        }
+        return nextDayState;
+    }
+
+    private static void flipIfNeeded(Map<Point, Point> tileState, Map<Point, Point> nextDayState, Point p) {
+        if (p.nextStateDetermined) {
+            return;
+        }
+
+        List<Point> neighbors = new ArrayList<>();
+        for (String direction : DIRECTIONS) {
+            Point neighborTile = new Point(p.x, p.y).move(direction);
+            neighbors.add(tileState.getOrDefault(neighborTile, neighborTile));
+        }
+
+        int blackTileNeighborCount = countBlackTiles(neighbors);
+        if (!p.isBlack() && blackTileNeighborCount == 0) {
+            // stop recursively processing neighbor tiles if all are white
+            return;
+        }
+
+        // first flip black to white
+        Point nextState = p.copyForNextState();
+        tileState.put(p, p); // label original tile "next state determined"
+
+        if (p.isBlack()) {
+            if (blackTileNeighborCount == 0 || blackTileNeighborCount > 2) {
+                nextState.flipToWhite();
+            }
+        } else {
+            // flip white to black
+            if (blackTileNeighborCount == 2) {
+                nextState.flipToBlack();
+            }
+        }
+        nextDayState.put(nextState, nextState);
+
+        for (Point neighbor : neighbors) {
+            flipIfNeeded(tileState, nextDayState, neighbor);
+        }
+    }
+
+    private static int countBlackTiles(Collection<Point> neighbors) {
+        return (int) neighbors.stream().filter(Point::isBlack).count();
     }
 
     private static class Point {
         private int x = 0;// starts at 0 in reference row, 1 at below (increments by 2 within row)
         private int y = 0;
+        private boolean isBlack = false;
+        private boolean nextStateDetermined = false;
 
         public Point() {
         }
 
-        public void move(String direction) {
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        private Point(int x, int y, boolean isBlack) {
+            this.x = x;
+            this.y = y;
+            this.isBlack = isBlack;
+        }
+
+        public Point copyForNextState() {
+            nextStateDetermined = true;
+            return new Point(x, y, isBlack);
+        }
+
+        public Point move(String direction) {
             switch (direction) {
                 case "e"  -> x += 2;
                 case "se" -> { x += 1; y -= 1;}
@@ -56,7 +136,24 @@ public class Day24Part02 {
                 case "nw" -> { x -= 1; y += 1;}
                 case "ne" -> { x += 1; y += 1;}
             }
+            return this;
         }
+
+        public Point flipToBlack() {
+            isBlack = true;
+            return this;
+        }
+
+        public Point flipToWhite() {
+            isBlack = false;
+            return this;
+        }
+
+        public boolean isBlack() {
+            return isBlack;
+        }
+
+
 
         @Override
         public boolean equals(Object o) {
@@ -70,5 +167,14 @@ public class Day24Part02 {
         public int hashCode() {
             return Objects.hash(x, y);
         }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+
     }
 }
