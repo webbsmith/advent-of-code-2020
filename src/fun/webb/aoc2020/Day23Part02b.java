902e13e866a020b09ba328b8013ef0f694be6cbc
webb <webbsemail@gmail.com>
2020-12-29 10:04:59 -0500

    implemented Winston's faster array linked list solution

--- src/fun/webb/aoc2020/Day23Part02b.java
+++ src/fun/webb/aoc2020/Day23Part02b.java
@@ -4,77 +4,91 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-// Implementing Winston's array linked list solution
+// Faster solution to https://adventofcode.com/2020/day/23 part 2
 //
+// Implementation of Winston's array linked list solution.
+//
+// If the circle is:
+//   2 8 4 5 7 3 9 6 1
+// then the data is stored as
+// data:  X 2 8 9 5 7 1 3 4 6
+// idx :  0 1 2 3 4 5 6 7 8 9
+
 public class Day23Part02b {
+
+    public static final int HIGHEST_VALUE = 1_000_000;
+    public static final int MOVES = 10_000_000;
+
     public static void main(String[] args) throws IOException {
-        String input = Files.readAllLines(Path.of("inputs/day23.txt")).get(0);
-        int moves = 10_000_000;
+        int[] cups = initializeCups("inputs/day23.txt");
 
-        // Each entry in the array holds the index of the next entry
-        List<Integer> cups = new ArrayList<>();
-        for (int i = 0; i < input.length(); i++) {
-            cups.add(Integer.parseInt(input.substring(i, i + 1)));
-        }
+        int nextCup = cups[HIGHEST_VALUE]; // Start with the first cup (which cups[HIGHEST_VALUE] points to)
+        for (int m = 0; m < MOVES; m++) {
+            int cup = nextCup;
 
-        int highestValue = 1_000_000;
-        for (int n = 10; n <= 1_000_000; n++) {
-            cups.add(n);
-        }
+            int firstCupToMove = cups[cup];
+            int midCupToMove = cups[firstCupToMove];
+            int lastCupToMove = cups[midCupToMove];
 
-        int size = highestValue;
-        for (int m = 0, currentIdx = 0; m < moves; m++, currentIdx = (currentIdx + 1) % size) {
-            System.out.println(m);
-            int cup = cups.get(currentIdx);
-            int pickupIdx = (currentIdx + 1) % cups.size();
-            List<Integer> pickedUp = new ArrayList<>();
-            pickedUp.add(cups.remove(pickupIdx));
-            if (pickupIdx != 0) pickupIdx %= cups.size();
-            pickedUp.add(cups.remove(pickupIdx));
-            if (pickupIdx != 0) pickupIdx %= cups.size();
-            pickedUp.add(cups.remove(pickupIdx));
-
-            int cupToInsertAfter = cup;
-            boolean positionFound = false;
-            while (!positionFound) {
-                cupToInsertAfter--;
-                if (cupToInsertAfter < 1) {
-                    cupToInsertAfter = highestValue;
-                }
-                if (pickedUp.contains(cupToInsertAfter)) {
-                    continue;
-                }
-
-                for (int i = 0; i < cups.size(); i++) {
-                    if (cups.get(i) == cupToInsertAfter) {
-                        cups.addAll(i + 1, pickedUp);
-                        positionFound = true;
-                        if (i < currentIdx) {
-                            // if cups were inserted in front, continue with the next cup
-                            if (currentIdx < size - 3) {
-                                currentIdx += 3;
-                            } else if (currentIdx == size - 3) {
-                                currentIdx += 2;
-                            } else if (currentIdx == size - 2) {
-                                currentIdx += 1;
-                            }
-                        }
-                        break;
-                    }
-                }
+            // Determine destination for cups to move
+            int dest = cup - 1;
+            if (dest < 1) dest = HIGHEST_VALUE;
+            while (dest == firstCupToMove || dest == midCupToMove || dest == lastCupToMove) {
+                dest--;
+                if (dest < 1) dest = HIGHEST_VALUE;
             }
+
+            // Get pointer from destination (destination's original next cup)
+            int destinationNextCup = cups[dest];
+
+            // First point the current cup to the one after the group that is moving
+            cups[cup] = cups[lastCupToMove];
+            // Then insert the cups at the destination
+            cups[lastCupToMove] = destinationNextCup;
+            cups[dest] = firstCupToMove;
+
+            nextCup = cups[cup];
         }
 
-        long product = 1;
-        int idxOf1 = cups.indexOf(1);
-        if (idxOf1 == highestValue - 1) {
-            idxOf1 = -1;
-        }
-        for (int i = idxOf1 + 1; i < idxOf1 + 3; i = (i + 1) % size) {
-            product *= cups.get(i);
-        }
-        System.out.println(product);
+        System.out.println((long)cups[1] * (long)cups[cups[1]]);
     }
+
+    private static int[] initializeCups(String file) throws IOException {
+        String input = Files.readAllLines(Path.of(file)).get(0);
+
+        // Add data from input file
+        // Each entry in the array holds the index of the next entry
+        int[] cups = new int[HIGHEST_VALUE + 1];
+        int firstCup = Integer.parseInt(input.substring(0, 1)); // Save first cup to point the last cup to
+        int nextIdx = firstCup; // Store the next entry at the value of the first cup
+        for (int i = 1; i < input.length(); i++) {
+            int val = Integer.parseInt(input.substring(i, i + 1));
+            cups[nextIdx] = val;
+            nextIdx = val;
+        }
+
+        // Add numbers 10 through 1 million
+        for (int n = 10; n <= HIGHEST_VALUE; n++) {
+            cups[nextIdx] = n;
+            nextIdx = n;
+        }
+        // At this point cups[999,999] -> 1,000,000
+        // nextIdx = 1,000,000
+        cups[nextIdx] = firstCup; // Complete the circle by pointing back to the beginning
+        return cups;
+    }
+
+    private static void printCups(int[] cups) {
+        StringBuilder sb = new StringBuilder();
+        int next = 1;
+        for (int i = 1; i < cups.length; i++) {
+            sb.append(cups[next]).append(" ");
+            next = cups[next];
+        }
+        System.out.println(sb);
+    }
+
 }
